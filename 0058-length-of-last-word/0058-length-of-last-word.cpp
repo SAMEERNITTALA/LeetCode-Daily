cce49e4381f47230b328f6aae73dd7451ddaa5d0
SAMEERNITTALA <158540853+SAMEERNITTALA@users.noreply.github.com>
2024-08-15 01:17:14 +0530

    Time: 0 ms (100%), Space: 8.7 MB (5.36%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) 
+    {
+        stack<string> st;
+        stringstream ss(s);
+        string word;
+        while (ss >> word) 
+        {
+            st.push(word);
+        }
+        return st.top().size();
+    }
+};
