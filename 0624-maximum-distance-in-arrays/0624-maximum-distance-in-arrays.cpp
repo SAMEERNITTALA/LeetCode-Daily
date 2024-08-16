ae8fa2dccd30d3c615d975d8e723543c5a9ebab1
SAMEERNITTALA <158540853+SAMEERNITTALA@users.noreply.github.com>
2024-08-16 13:28:45 +0530

    Time: 217 ms (90.57%), Space: 107.9 MB (57.55%) - LeetHub

--- 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.cpp
+++ 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.cpp
@@ -5,7 +5,8 @@ public:
         int biggest = arrays[0].back();
         int max_distance = 0;
 
-        for (int i = 1; i < arrays.size(); ++i) {
+        for (int i = 1; i < arrays.size(); ++i) 
+        {
             max_distance = max(max_distance, abs(arrays[i].back() - smallest));
             max_distance = max(max_distance, abs(biggest - arrays[i][0]));
             smallest = min(smallest, arrays[i][0]);
