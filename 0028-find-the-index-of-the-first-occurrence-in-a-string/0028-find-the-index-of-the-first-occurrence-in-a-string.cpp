940097475a6cc0b259151082ffef84a81c00590b
SAMEERNITTALA <158540853+SAMEERNITTALA@users.noreply.github.com>
2024-08-16 15:22:30 +0530

    Time: 4 ms (16.45%), Space: 8.2 MB (13%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
-    int strStr(string haystack, string needle) {
-        if (haystack.length() < needle.length()) {
-            return -1;
+    int strStr(const string& haystack, const string& needle) 
+    {
+        if (needle.empty()) return 0;
+        if (needle.size() > haystack.size()) return -1;
+        for (int i = 0; i <= haystack.size() - needle.size(); ++i) 
+        {
+            if (haystack.substr(i, needle.size()) == needle) return i;
         }
-        
-        for (int i = 0; i <= haystack.length(); i++) {
-            if (haystack.substr(i, needle.length()) == needle) {
-                return i;
-            }
-        }
-        
-        return -1;        
+        return -1;
     }
 };
