114339675b4dfcdfc50c1f060f2b2a73bb962687
SAMEERNITTALA <158540853+SAMEERNITTALA@users.noreply.github.com>
2024-08-20 23:01:25 +0530

    Time: 4 ms (39.04%), Space: 9 MB (7.62%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
+    void transpose(vector<vector<int>>& m)
+    {
+        int n=m.size();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(j>i)
+                {
+                    swap(m[i][j],m[j][i]);
+                }
+            }
+        }
+    }
     void rotate(vector<vector<int>>& matrix) {
-        int m = matrix.size();
-        int n = matrix[0].size();
-
-        // First, take the transpose of the matrix
-        for(int i = 0; i < m; i++) {
-            for(int j = i; j < n; j++) {
-                swap(matrix[i][j], matrix[j][i]);
+        transpose(matrix);
+        int n=matrix.size();
+        int i=0,j=n-1;
+        while(i<j)
+        {
+            for(int k=0;k<n;k++)
+            {
+                swap(matrix[k][i],matrix[k][j]);
             }
+            i++;
+            j--;
         }
-
-        // Then, reverse each row
-        for(int i = 0; i < m; i++) {
-            reverse(matrix[i].begin(), matrix[i].end());
-        }
-        
-        // Printing matrix
-        cout << "[";
-        for(int i = 0; i < m; i++) {
-            cout << "[";
-            for(int j = 0; j < n; j++) {
-                cout << matrix[i][j];
-                if(j < n-1) cout << ",";
-            }
-            cout << "]";
-            if(i < n-1) cout << ",";
-        }
-        cout << "]";
     }
 };
