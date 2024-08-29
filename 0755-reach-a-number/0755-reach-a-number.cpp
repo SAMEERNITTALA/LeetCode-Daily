228f0c66de0ebb54f5c7e6de34686c5eb9d7fed0
SAMEERNITTALA <158540853+SAMEERNITTALA@users.noreply.github.com>
2024-08-30 01:43:03 +0530

    Time: 3 ms (49.82%), Space: 7.4 MB (64.15%) - LeetHub

--- 0755-reach-a-number/0755-reach-a-number.cpp
+++ 0755-reach-a-number/0755-reach-a-number.cpp
@@ -1,38 +1,16 @@
 class Solution {
 public:
     int reachNumber(int target) {
-        int i = 1;
-        int ans = 0;
-        int t = abs(target);
-        long int x = 0;
-        while(i*(i+1)/2 < t)
-        {
-            i++;
+        int t = abs(target);  // Work with the absolute value of target
+        int steps = 0;
+        int sum = 0;
+
+        // Keep moving until the sum is at least equal to target
+        while (sum < t || (sum - t) % 2 != 0) {
+            steps++;
+            sum += steps;
         }
-        x = i;
-        if(x % 4 == 1 || x % 4 == 2)//odd
-        {
-            if(t % 2 == 1) ans = x;//odd
-            else//even
-            {
-                while(x % 4 == 1 || x % 4 == 2){
-                    x++;
-                }
-                ans = x;
-            }
-        }
-        else//even
-        {
-            if(t % 2 == 0) ans = x;//even
-            else//odd
-            {
-                while(x % 4 == 0 || x % 4 == 3)
-                {
-                    x++;
-                }
-                ans = x;
-            }
-        }
-        return ans; 
+
+        return steps;
     }
 };
