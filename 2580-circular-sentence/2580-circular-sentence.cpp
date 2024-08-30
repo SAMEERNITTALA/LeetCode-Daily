b7cca5ee711cad770ce774f5ae6c56039ca9da06
SAMEERNITTALA <158540853+SAMEERNITTALA@users.noreply.github.com>
2024-08-30 15:19:13 +0530

    Time: 0 ms (100%), Space: 9.3 MB (6.55%) - LeetHub

--- 2580-circular-sentence/2580-circular-sentence.cpp
+++ 2580-circular-sentence/2580-circular-sentence.cpp
@@ -2,19 +2,16 @@ class Solution {
 public:
     bool isCircularSentence(string sentence) {
         vector<string> v;
-        for(int i = 0; i < sentence.size(); i++)
-        {
+        for(int i = 0; i < sentence.size(); i++) {
             string s = "";
-            while(i < sentence.size() && sentence[i] != ' ')
-            {
+            while(i < sentence.size() && sentence[i] != ' ') {
                 s += sentence[i];
                 i++;
             }
             v.emplace_back(s);
         }
         int count = 0;
-        for(int i = 1; i < v.size(); i++)
-        {
+        for(int i = 1; i < v.size(); i++) {
             if(v[i-1].back() == v[i][0]) count++;
         }
         if(v[0][0] == v[v.size() - 1].back()) count++;
