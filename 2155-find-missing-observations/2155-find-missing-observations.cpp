1d2dd04462c84a40896e8d6d1ac1fd3e401a6400
SAMEERNITTALA <158540853+SAMEERNITTALA@users.noreply.github.com>
2024-09-05 12:45:26 +0530

    Time: 103 ms (37.5%), Space: 126.5 MB (8.58%) - LeetHub

--- 2155-find-missing-observations/2155-find-missing-observations.cpp
+++ 2155-find-missing-observations/2155-find-missing-observations.cpp
@@ -7,26 +7,14 @@ public:
         for(int r : rolls) sum += r;
         int value = mean * (m + n) - sum;
         if(value > 6 * n || mean > 6 || value < n) return {};
-        // else if(value == 6 * n)
-        // {
-        //     for(int i = 0; i < n; i++) v.emplace_back(6);
-        // }
         else
         {
             int x = value / n;
             int y = value % n;
-            if(x + y <= 6)
+            for(int i = 0; i < n; i++) v.emplace_back(x);
+            for(int i = 0; i < y; i++)
             {
-                for(int i = 0; i < n-1; i++) v.emplace_back(x);
-                v.emplace_back(x+y);
-            }
-            else
-            {
-                for(int i = 0; i < n; i++) v.emplace_back(x);
-                for(int i = 0; i < y; i++)
-                {
-                    v[i] += 1;
-                }
+                v[i] += 1;
             }
         }
         return v;
