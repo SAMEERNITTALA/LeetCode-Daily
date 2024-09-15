4a370494714a3f6476a0c67ea20a3326066cf60b
SAMEERNITTALA <158540853+SAMEERNITTALA@users.noreply.github.com>
2024-09-15 19:00:18 +0530

    Time: 0 ms (100%), Space: 13.3 MB (49.5%) - LeetHub

--- 0748-largest-number-at-least-twice-of-others/0748-largest-number-at-least-twice-of-others.cpp
+++ 0748-largest-number-at-least-twice-of-others/0748-largest-number-at-least-twice-of-others.cpp
@@ -3,14 +3,12 @@ public:
     int dominantIndex(vector<int>& nums) {
         auto it = max_element(nums.begin(), nums.end());
         int ans = distance(nums.begin(), it);
-        int count = 0;
+        
         for(int i = 0; i < nums.size(); i++)
         {
             if(nums[i] == *it) continue;
             
-            else if(*it >= 2 * nums[i]) count++;
-
-            else return -1;
+            if(*it < 2 * nums[i]) return -1;
         }
 
         return ans;
