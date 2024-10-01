5219ee9fd8a378b9b4fe9e54e5805e684e2f3f8e
SAMEERNITTALA <158540853+SAMEERNITTALA@users.noreply.github.com>
2024-10-02 01:55:02 +0530

    Time: 12 ms (12.22%), Space: 13.3 MB (39.8%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.cpp
+++ 0414-third-maximum-number/0414-third-maximum-number.cpp
@@ -1,39 +1,13 @@
 class Solution {
 public:
     int thirdMax(vector<int>& nums) {
-       int first = nums[0];
-       int second = INT_MIN;
-       int third = INT_MIN;
-       int flag = 0;
-       if(nums[0]==INT_MIN){
-        flag =1;
-       }
+       set<int> s(nums.begin(), nums.end());
 
-        for(int i = 1; i < nums.size(); i++)
-        {
-             if(nums[i]==INT_MIN)
-                {
-                    flag=1;
-                }
-            if(nums[i] > first)
-            {
-               
-                third = second;
-                second = first;
-                first = nums[i];
-            }
-            else if(nums[i] < first && second < nums[i])
-            {
+       if(s.size() < 3) return *s.rbegin();
 
-                third = second;
-                second = nums[i];
-            }  
-            else if(nums[i] < second && third < nums[i]) 
-            {
-                third = nums[i];
-            }
-        }
-        if((third == INT_MIN && flag==0) || third==second) return first;
-        return third;
+       auto it = s.rbegin();
+       advance(it, 2);
+
+       return *it; 
     }
 };
