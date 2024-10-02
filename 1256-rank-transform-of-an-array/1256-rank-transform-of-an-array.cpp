1174e79f47b9cba1c925ee47bec82ee95dd05c64
SAMEERNITTALA <158540853+SAMEERNITTALA@users.noreply.github.com>
2024-10-02 09:31:14 +0530

    Time: 73 ms (52.92%), Space: 44.5 MB (32.37%) - LeetHub

---
+++ 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> arrayRankTransform(vector<int>& arr) {
+        if(arr.size() < 1) return arr;
+        vector<int> v;
+        vector<int> ar(arr.begin(), arr.end());
+        sort(ar.begin(), ar.end());
+        unordered_map<int, int> m;
+        m[ar[0]] = 1;
+        for(int i = 1; i < ar.size(); i++)
+        {
+            if(ar[i] == ar[i-1]) continue;
+            m[ar[i]] = m[ar[i-1]] + 1;
+        }
+        for(int i = 0; i < arr.size(); i++)
+        {
+            v.emplace_back(m[arr[i]]);
+        }
+        return v;
+    }
+};
