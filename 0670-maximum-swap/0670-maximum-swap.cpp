52165cfe66317f4f0fa45cb40194bc4849e0d192
SAMEERNITTALA <158540853+SAMEERNITTALA@users.noreply.github.com>
2024-10-17 23:15:47 +0530

    Time: 0 ms (100%), Space: 7.5 MB (70.34%) - LeetHub

---
+++ 0670-maximum-swap/0670-maximum-swap.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int maximumSwap(int num) {
+        string s = to_string(num);
+        if(s.size() == 1) return num;
+        for(int i = 0; i < s.size(); i++)
+        {
+            int large = s[i];
+            int largeIndex = i;
+            for(int j = i+1; j < s.size(); j++)
+            {
+                if((int)s[j] >= large)
+                {
+                    large = (int)s[j];
+                    largeIndex = j;
+                }
+            }
+            if(large != (int)s[i])
+            {
+                int temp = (int)s[i];
+                s[i] = s[largeIndex];
+                s[largeIndex] = temp;
+                break;
+            }
+        }
+        return stoi(s);
+    }
+};
