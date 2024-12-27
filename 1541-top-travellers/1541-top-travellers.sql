e3280070334fabb2d8420fdd3f613f98d10f343a
SAMEERNITTALA <158540853+SAMEERNITTALA@users.noreply.github.com>
2024-12-27 17:23:27 +0530

    Time: 1921 ms (5.62%), Space: 0B (100%) - LeetHub

--- 1541-top-travellers/1541-top-travellers.sql
+++ 1541-top-travellers/1541-top-travellers.sql
@@ -2,5 +2,5 @@
 select u.name, coalesce(sum(r.distance), 0) as travelled_distance 
 from Users u left join Rides r
 on u.id = r.user_id
-group by u.name, r.user_id
+group by u.id
 order by travelled_distance desc, u.name;
