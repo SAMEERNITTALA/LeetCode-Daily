4a438c2a9250d8abf354199950ba7d3003e68af8
SAMEERNITTALA <158540853+SAMEERNITTALA@users.noreply.github.com>
2024-12-29 06:07:50 +0530

    Time: 0 ms (100%), Space: 11 MB (52.63%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool check(vector<int>& nums) {
         int n = nums.size();
-        int m = 0;
+        /*int m = 0;
         for(int i = 1; i < n; i++)
         {
             if(nums[i] < nums[i-1])
@@ -14,6 +14,13 @@ public:
         reverse(nums.begin(), nums.begin() + m);
         reverse(nums.begin() + m, nums.end());
         reverse(nums.begin(), nums.end());
-        return is_sorted(nums.begin(), nums.end());
+        return is_sorted(nums.begin(), nums.end());*/
+        int count = 0;
+        for(int i = 0; i < n; i++)
+        {
+            if(nums[i] > nums[(i+1) % n]) count++;
+            if(count > 1) return false;
+        }
+        return true;
     }
 };
