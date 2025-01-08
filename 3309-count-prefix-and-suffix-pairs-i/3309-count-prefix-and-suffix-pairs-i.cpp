fed18ea39f7ffa4ec14d52b39ba737f76e4166f8
SAMEERNITTALA <158540853+SAMEERNITTALA@users.noreply.github.com>
2025-01-08 08:46:55 +0530

    Time: 0 ms (100%), Space: 23.5 MB (66.95%) - LeetHub

--- 3309-count-prefix-and-suffix-pairs-i/3309-count-prefix-and-suffix-pairs-i.cpp
+++ 3309-count-prefix-and-suffix-pairs-i/3309-count-prefix-and-suffix-pairs-i.cpp
@@ -3,16 +3,13 @@ public:
     bool isPrefixAndSuffix(const string& str1, const string& str2) {
         int a = str1.size();
         int b = str2.size();
-        
-        // Ensure str1 can be a prefix and suffix of str2
+
         if (a > b) return false;
 
-        // Check prefix
         for (int i = 0; i < a; i++) {
             if (str1[i] != str2[i]) return false;
         }
 
-        // Check suffix
         for (int i = 0; i < a; i++) {
             if (str1[i] != str2[b - a + i]) return false;
         }
@@ -22,13 +19,11 @@ public:
 
     int countPrefixSuffixPairs(vector<string>& words) {
         int count = 0;
-
-        // Compare each pair of words
-        for (int i = 0; i < words.size(); i++) {
-            for (int j = 0; j < words.size(); j++) {
-                if (i < j && isPrefixAndSuffix(words[i], words[j])) {
-                    count++;
-                }
+        for (int i = 0; i < words.size(); i++) 
+        {
+            for (int j = i+1; j < words.size(); j++) 
+            {
+                if (isPrefixAndSuffix(words[i], words[j])) count++;
             }
         }
         return count;
