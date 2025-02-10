e8d53de59fac1ef393c245d9ba10031d39e49f67
SAMEERNITTALA <158540853+SAMEERNITTALA@users.noreply.github.com>
2025-02-10 22:40:22 +0530

    Time: 0 ms (100%), Space: 9.1 MB (27.61%) - LeetHub

---
+++ 3447-clear-digits/3447-clear-digits.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+        stack<char> st;
+        for(int i = 0; i < s.size(); i++)
+        {
+            if(!isdigit(s[i])) st.push(s[i]);
+            else st.pop();
+        }
+        string ans = "";
+        while(!st.empty())
+        {
+            ans += st.top();
+            st.pop();
+        } 
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
