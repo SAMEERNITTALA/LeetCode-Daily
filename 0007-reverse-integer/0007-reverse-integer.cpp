90bda12a4baf162fbb9cbabd6ec46f2367271d47
SAMEERNITTALA <158540853+SAMEERNITTALA@users.noreply.github.com>
2025-02-10 18:09:52 +0530

    Time: 0 ms (100%), Space: 8.4 MB (82.46%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,31 +1,18 @@
 class Solution {
 public:
     int reverse(int x) {
-        // Determine the sign of x
-        int sign = (x < 0) ? -1 : 1;
-        x = abs(x);
-        
-        // Vector to store the digits
-        vector<int> v;
-        while(x > 0) {
+        int ans = 0;
+
+        while(x != 0)
+        {
             int a = x % 10;
-            v.emplace_back(a); // Add all digits, including zeros
+
+            if((ans > INT_MAX / 10) || (ans < INT_MIN / 10)) return 0;
+
+            ans = ans * 10 + a;
             x = x / 10;
         }
-        
-        // Reverse the digits and construct the reversed number
-        int num = 0;
-        for(int i = 0; i < v.size(); i++) {
-            // Check for overflow before multiplying by 10
-            if (num > (INT_MAX - v[i]) / 10) {
-                return 0; // Return 0 if overflow occurs
-            }
-            num = (num * 10) + v[i];
-        }
-        
-        // Reapply the sign
-        num *= sign;
-        
-        return num;
+
+        return ans;
     }
 };
