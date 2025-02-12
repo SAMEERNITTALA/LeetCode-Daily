744826555e3f7286ea5b063b8acf84aab007463a
SAMEERNITTALA <158540853+SAMEERNITTALA@users.noreply.github.com>
2025-02-13 00:19:49 +0530

    Time: 47 ms (29.08%), Space: 75.4 MB (20.24%) - LeetHub

---
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int genSum(int num) {
+        int ans = 0;
+        while(num != 0)
+        {
+            int a = num % 10;
+            ans += a;
+            num = num / 10;
+        }
+        return ans;
+    }
+    
+    int maximumSum(vector<int>& nums) {
+        int ans = INT_MIN;
+        unordered_map<int, vector<int>> m;
+        for(int num : nums) m[genSum(num)].emplace_back(num);
+        for(auto pair : m)
+        {
+            if(pair.second.size() >= 2)
+            {
+                sort(pair.second.begin(), pair.second.end(), greater<int>());
+                ans = max(ans, pair.second[0] + pair.second[1]);
+            }
+        }
+        if(ans == INT_MIN) return -1;
+        return ans;
+    }
+};
