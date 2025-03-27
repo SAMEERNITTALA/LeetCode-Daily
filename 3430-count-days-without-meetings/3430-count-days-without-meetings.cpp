bdc2360858ea30a297be4f5fd59d55f17398f9c8
SAMEERNITTALA <158540853+SAMEERNITTALA@users.noreply.github.com>
2025-03-27 23:52:21 +0530

    Time: 71 ms (56.16%), Space: 126.6 MB (36.98%) - LeetHub

---
+++ 3430-count-days-without-meetings/3430-count-days-without-meetings.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countDays(int days, vector<vector<int>>& meetings) {
+        sort(meetings.begin(), meetings.end());
+        int ans = 0;
+        ans += meetings[0][0] - 1;
+        for(int i = 1; i < meetings.size(); i++)
+        {
+            if(meetings[i][0] > meetings[i-1][1]) ans += meetings[i][0] - meetings[i-1][1] - 1;
+            else meetings[i][1] = max(meetings[i-1][1], meetings[i][1]);
+        }
+        ans += days - meetings.back()[1];
+        return ans;
+    }
+};
