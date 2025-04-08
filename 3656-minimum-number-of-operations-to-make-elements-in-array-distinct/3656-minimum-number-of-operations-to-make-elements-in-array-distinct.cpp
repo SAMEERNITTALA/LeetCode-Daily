815a8d8323405c90abcc137e4fbe00e0030a46b9
SAMEERNITTALA <158540853+SAMEERNITTALA@users.noreply.github.com>
2025-04-08 06:11:47 +0530

    Time: 54 ms (5.8%), Space: 47.1 MB (5.63%) - LeetHub

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
@@ -10,18 +10,9 @@ public:
         int ans = 0;
         for(int i = 0; i < nums.size(); i+= 3)
         {
-            /*if(nums.size() - i >= 3)
-            {
-                vector<int> v(nums.begin() + i, nums.end());
-                if(compare(v)) return ans;
-                else ans += 1;
-
-            }
-            else return ans + 1;*/
             vector<int> v(nums.begin() + i, nums.end());
             ans += int(!compare(v));
         }
-        //return -1;
         return ans;
     }
 };
