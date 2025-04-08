31645e5f7eb6e5d0825f7eadc23a857b0ebcc27f
SAMEERNITTALA <158540853+SAMEERNITTALA@users.noreply.github.com>
2025-04-08 06:22:44 +0530

    Time: 11 ms (23.04%), Space: 31.5 MB (18.6%) - LeetHub

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
@@ -1,18 +1,39 @@
 class Solution {
 public:
-    bool compare(vector<int>& v) {
+    /*bool compare(vector<int>& v) {
         unordered_map<int, int> m;
         for(int n : v) m[n]++;
         return m.size() == v.size();
-    }
+    }*/
 
     int minimumOperations(vector<int>& nums) {
-        int ans = 0;
+        /*int ans = 0;
         for(int i = 0; i < nums.size(); i+= 3)
         {
             vector<int> v(nums.begin() + i, nums.end());
             ans += int(!compare(v));
         }
+        return ans;*/
+        int n = nums.size(), i = 0, ans = 0;
+        while(i < n)
+        {
+            unordered_set<int> s;
+            bool hasDuplicate = false;
+            for(int j = i; j < n; j++)
+            {
+                if(s.count(nums[j]))
+                {
+                    hasDuplicate = true;
+                    break;
+                }
+                s.insert(nums[j]);
+            }
+            if(!hasDuplicate) break;
+
+            i += 3;
+            ans++; 
+        }
+
         return ans;
     }
 };
