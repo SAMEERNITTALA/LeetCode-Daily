331e192ff45bcf10eef9589a0cc8f03d6723f20e
SAMEERNITTALA <158540853+SAMEERNITTALA@users.noreply.github.com>
2025-05-28 21:31:25 +0530

    Time: 3 ms (32.46%), Space: 16.1 MB (9.14%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,17 +1,24 @@
 class Solution {
 public:
+    vector<vector<int>> f(int ind, vector<int>& v, vector<int>& nums, int n) {
+        if (ind == n) return {v};  // Base case: return current subset
+        
+        // Include nums[ind]
+        v.push_back(nums[ind]);
+        vector<vector<int>> left = f(ind + 1, v, nums, n);
+        v.pop_back();
+        
+        // Exclude nums[ind]
+        vector<vector<int>> right = f(ind + 1, v, nums, n);
+        
+        // Combine both
+        left.insert(left.end(), right.begin(), right.end());
+        return left;
+    }
+
     vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int> v;
         int n = nums.size();
-        vector<vector<int>> v;
-        for(int i = 0; i < 1 << n; i++)
-        {
-            vector<int> ans;
-            for(int j = 0; j < n; j++)
-            {
-                if((1 << j) & i) ans.emplace_back(nums[j]);
-            }
-            v.emplace_back(ans);
-        }
-        return v;
+        return f(0, v, nums, n);
     }
 };
