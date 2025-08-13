e708868e9f601aa865b97bf45e8830cdbf065817
SAMEERNITTALA <158540853+SAMEERNITTALA@users.noreply.github.com>
2025-08-13 11:39:43 +0530

    Time: 4 ms (47.44%), Space: 9 MB (46.41%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -3,7 +3,7 @@ public:
     bool isPowerOfThree(int n) {
         if(n <= 0) return false;
         int rem = 0;
-        while(abs(n) > 1)
+        while(n > 1)
         {
             rem += n % 3;
             n = n / 3;
